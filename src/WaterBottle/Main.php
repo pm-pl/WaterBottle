a96f5c8d9ae0b9c76e3345a7e62044e7ae51b400
bonbionseker <46757959+bonbionTR@users.noreply.github.com>
2023-05-06 10:34:48 +0300

    Config Update
    Yazıları değiştirmek için config eklendi!

--- src/WaterBottle/Main.php
+++ src/WaterBottle/Main.php
@@ -6,12 +6,18 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener {
 
+    private $config;
+
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     }    
 
     public function onPlayerInteract(PlayerInteractEvent $event) {
@@ -22,13 +28,18 @@ class Main extends PluginBase implements Listener {
         if($item->getId() === ItemIds::GLASS_BOTTLE && $block->getId() === ItemIds::WATER) {
             if($player->getInventory()->canAddItem(ItemFactory::getInstance()->get(ItemIds::POTION, 0, 1))) {
                 $item = ItemFactory::getInstance()->get(ItemIds::POTION, 0, 1);
-                $item->setCustomName("§r§bSu Şişesi");
+                $item->setCustomName($this->config->getNested("items.potion_name"));
                 $player->getInventory()->addItem($item);
-                $player->sendMessage("§9Bir su şişesi doldurdun!");
+                $player->sendMessage($this->config->getNested("messages.filled_bottle"));
                 
+                $pk = new LevelSoundEventPacket();
+                $pk->sound = LevelSoundEventPacket::SOUND_WATER;
+                $pk->position = $player->getPosition();
+                $player->sendDataPacket($pk);
+
                 $player->getInventory()->removeItem(ItemFactory::getInstance()->get(ItemIds::GLASS_BOTTLE, 0, 1));
             } else {
-                $player->sendMessage("§cEyvah, Envanterin dolu, su şişesi dolduramazsın!");
+                $player->sendMessage($this->config->getNested("messages.full_inventory"));
             }
         }        
 
